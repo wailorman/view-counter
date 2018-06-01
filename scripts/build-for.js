18776f022ccce708cb1c811e91a70e5819782059
Sergey Popov <wailorman@gmail.com>
2018-06-01 16:47:11 +0300

    feat: WIP build-for

--- scripts/build-for.js
+++ scripts/build-for.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
-const fse = require('fs-extra');
+// const fse = require('fs-extra');
 const path = require('path');
+// const util = require('util');
+// const babel = require('@babel/core');
+
+// const execFile = util.promisify(require('child_process').execFile);
 
 (async () => {
   const PATHS = {
     babelConfig: path.resolve(__dirname, '../.babelrc'),
-    tmpBabelConfig: path.resolve(__dirname, '../.tmp/.babelrc'),
+    entryFile: path.resolve(__dirname, '../src/index.js'),
+    distFile: path.resolve(__dirname, '../lib/index.js'),
+    babelBin: path.resolve(__dirname, '../node_modules/.bin/babel'),
   };
 
   const DESIRED_NODE_VERSION = process.argv[2] || '6';
@@ -16,7 +22,13 @@ const path = require('path');
 
   newBabelConfig.presets[0][1].targets.node = DESIRED_NODE_VERSION;
 
-  await fse.outputFile(PATHS.tmpBabelConfig, JSON.stringify(newBabelConfig, null, 2));
+  // await fse.outputFile(PATHS.tmpBabelConfig, JSON.stringify(newBabelConfig, null, 2));
+
+  // const { stdout } = await execFile('node', [PATHS.babelBin]);
+
+  // const { code } = babel.transformFileSync(PATHS.entryFile, newBabelConfig);
+
+  // debugger;
 
   console.log('Success!');
 })().catch(console.error);
